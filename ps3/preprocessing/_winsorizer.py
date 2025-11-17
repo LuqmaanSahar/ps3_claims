4a40502a389c18cc733f5faa85aae81f3938611a
LSahar <150852718+LuqmaanSahar@users.noreply.github.com>
2025-11-17 16:54:18 +0000

    exercise 2

--- ps3/preprocessing/_winsorizer.py
+++ ps3/preprocessing/_winsorizer.py
@@ -5,11 +5,35 @@ from sklearn.utils.validation import check_is_fitted
 # TODO: Write a simple Winsorizer transformer which takes a lower and upper quantile and cuts the
 # data accordingly
 class Winsorizer(BaseEstimator, TransformerMixin):
-    def __init__(self):
-        pass
+    """
+    Winsorizes (clips) the data between a specified upper and lower quantile
+
+    Parameters
+    ----------
+    lower_quantile : float
+        Training data
+    upper_quantile : float
+        Name of ID column
+    X : pd.DataFrame
+        Feature matrix
+
+    Returns
+    -------
+    X_winsorized
+        Winsorized (clipped) feature matrix
+    """
+    def __init__(self, lower_quantile, upper_quantile):
+        self.lower_quantile = lower_quantile
+        self.upper_quantile = upper_quantile
 
     def fit(self, X, y=None):
-        pass
+        self.lower_quantile_ = np.quantile(X, self.lower_quantile, axis=0)
+        self.upper_quantile_ = np.quantile(X, self.upper_quantile, axis=0)
+
+        return self
 
     def transform(self, X):
-        pass
+        X_winsorized = np.clip(X, self.lower_quantile_, self.upper_quantile_)
+
+        return X_winsorized
+        
--- tests/preprocessing/test_winsorizer.py
+++ tests/preprocessing/test_winsorizer.py
@@ -4,11 +4,40 @@ import pytest
 from ps3.preprocessing import Winsorizer
 
 # TODO: Test your implementation of a simple Winsorizer
+
+# define the parameters to test with
 @pytest.mark.parametrize(
     "lower_quantile, upper_quantile", [(0, 1), (0.05, 0.95), (0.5, 0.5)]
 )
+
+
 def test_winsorizer(lower_quantile, upper_quantile):
 
-    X = np.random.normal(0, 1, 1000)
+    X = np.random.normal(0, 1, 1000)    # example feature matrix
 
-    assert False
+    test_win = Winsorizer(lower_quantile=lower_quantile,
+                        upper_quantile=upper_quantile)
+
+    test_win.fit(X)
+    
+    # Get the learned limits 
+    lower_limits = test_win.lower_quantile_
+    upper_limits = test_win.upper_quantile_
+
+    # Transform the data
+    X_transformed = test_win.transform(X)
+
+    
+    # Assert that all transformed values are >= the learned lower limit
+    assert np.all(X_transformed >= lower_limits)
+    
+    # Assert that all transformed values are <= the learned upper limit
+    assert np.all(X_transformed <= upper_limits)
+
+    # Compute expected upper and lower quantiles for given X
+    expected_lower = np.quantile(X, lower_quantile, axis=0)
+    expected_upper = np.quantile(X, upper_quantile, axis=0)
+    
+    # Assert that the correct limits were learned
+    assert np.allclose(lower_limits, expected_lower)
+    assert np.allclose(upper_limits, expected_upper)
